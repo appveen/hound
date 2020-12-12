9d4f14dc19cb68709f3a42216ac0e46cc5014314
Jerry M <developer.jerrymannel@gmail.com>
2020-12-12 13:42:36 +0530

    nodejs- mongo connection tests

---
+++ app.js
@@ -0,0 +1,18 @@
+"use strict"
+const log4js = require("log4js")
+const config = require("./config")
+log4js.configure(config.logging.options);
+
+let version = require("./package.json").version
+let log = log4js.getLogger(`[${process.env.HOSTNAME || "hound"}] [HOUND ${version}]`)
+log.level = config.logging.loglevel
+global.logger = log
+
+let mongo = require("./lib/mongo.lib")
+let mongoose = require("./lib/mongoose.lib")
+
+async function start() {
+	await mongo.test()
+	// await mongoose.test()
+}
+start()
---
+++ config.js
@@ -0,0 +1,58 @@
+const os = require("os")
+const mongodb = require("mongodb")
+
+module.exports = {
+	"hostname": os.hostname(),
+	"server": {
+		"port": process.env.PORT || 18080
+	},
+	"db": {
+		"author": {
+			"url": process.env.MONGO_AUTHOR_URL || 'mongodb://localhost:27017',
+			"dbName": process.env.MONGO_AUTHOR_DBNAME || "odpConfig"
+		},
+		"appcenter": {
+			"url": process.env.MONGO_APPCENTER_URL || 'mongodb://localhost:27017',
+			"dbName": process.env.MONGO_APPCENTER_DBNAME || "appveen-adam"
+		},
+		"logs": {
+			"url": process.env.MONGO_LOGS_URL || 'mongodb://localhost:27017',
+			"dbName": process.env.MONGO_LOGS_DBNAME || "odpLogs"
+		},
+		"mongoReconnectiontime": process.env.MONGO_RECONN_TIME || "1000",
+		"mongoReconnectionRetries": process.env.MONGO_RECONN_TRIES || "1",
+		"mongooseOptions": {
+			"reconnectTries": process.env.MONGO_RECONN_TRIES,
+			"reconnectInterval": process.env.MONGO_RECONN_TIME,
+			"dbName": process.env.MONGO_AUTHOR_DBNAME || 'odpConfig',
+			"useNewUrlParser": true
+		},
+		"mongoUtilsOptions": {
+			"defaults": { "filter": { '_metadata.deleted': false } },
+			"numberOfRetries": process.env.MONGO_RECONN_TRIES || "1",
+			"retryMiliSeconds": process.env.MONGO_RECONN_TIME || "500",
+			"useNewUrlParser": true
+		},
+		"mongoOptions": {
+			"readPreference": mongodb.ReadPreference.SECONDARY_PREFERRED,
+			"numberOfRetries": process.env.MONGO_RECONN_TRIES || "1",
+			"useUnifiedTopology": true
+		}
+	},
+	"logging": {
+		"loglevel": process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "info",
+		"options": {
+			"appenders": {
+				"out": {
+					"type": 'stdout'
+				}
+			},
+			"categories": {
+				"default": {
+					"appenders": ['out'],
+					"level": 'error'
+				}
+			}
+		}
+	},
+}
